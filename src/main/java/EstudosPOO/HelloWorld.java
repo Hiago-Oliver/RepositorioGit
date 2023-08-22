4ec177be5ab77c01975bc2a0877f4f3f0488a0bc
Hiago-Oliver <hiago.oliver.12@gmail.com>
2023-08-22 18:26:31 -0300

    Hello World!

---
+++ src/main/java/EstudosPOO/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EstudosPOO;
+
+/**
+ *
+ * @author hiago
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
